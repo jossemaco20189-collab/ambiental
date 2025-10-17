2e437f97cce74bab81f7aa7714203c69e84bcac5
jossemaco20189-collab <jossemaco.20189@gmail.com>
2025-10-17 17:18:55 -0300

    Update quiz_ambiente.py

--- quiz_ambiente.py
+++ quiz_ambiente.py
@@ -261,5 +261,6 @@ if st.session_state.get('finished'):
         
         col1, col2, col3 = st.columns([1,1,1])
         with col1:
-            if st.button('🎉'
+            if st.button('🎉')
+
 
